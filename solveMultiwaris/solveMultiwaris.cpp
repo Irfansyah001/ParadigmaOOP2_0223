3eadbbbe7a9972437ae270dc47a58a698a686364
Irfansyah001 <syahirfan545@gmail.com>
2024-05-28 15:00:18 +0700

    add class budi : public pekerja, public pelajar, cout Budi dibuat, add budi a(12) dan return 0 pada int main

--- solveMultiwaris/solveMultiwaris.cpp
+++ solveMultiwaris/solveMultiwaris.cpp
@@ -31,6 +31,20 @@ public:
 		cout << "pelajar dibuat\n" << endl;
 	}
 };
-int main()
-{   
+
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "Budi dibuat\n" << endl;
+	}
+};
+
+int main() {   
+	budi a(12);
+
+	return 0;
 }
